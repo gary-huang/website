b9058c14d7a90cf0bf593c9860b87ac4f397cf74
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-03-27 04:59:03 -0400

    prod: add allowed hosts

--- crossroads/settings/prod.py
+++ crossroads/settings/prod.py
@@ -5,6 +5,11 @@ DEBUG = False
 
 SECRET_KEY = os.getenv("DJ_SECRET")
 
+ALLOWED_HOSTS = [
+    "crossroadschurch.ajax",
+    "crossroadsinajax.xyz",
+]
+
 try:
     from .local import *
 except ImportError:
