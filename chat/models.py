50962939ff8823f812fad5fdc53be8a469b49bdb
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-03-29 04:41:23 -0400

    chat: add another emoji

--- chat/models.py
+++ chat/models.py
@@ -29,7 +29,7 @@ class ChatMessage(models.Model):
         # Aggregate common reacts into a list of tuples [(emoji, count)]
         aggr = []
         reacts = self.reacts.all()
-        for react in "🙏,🙌,👋,➕".split(","):
+        for react in "🙏,🙌,👋,➕,🤣".split(","):
             aggr.append((react, len(reacts.filter(type=react))))
         return aggr
 
