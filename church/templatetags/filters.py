ecca2f00a269d6495b2bbcf7d17624bf44fe7b7f
Kyle Verhoog <kyle.verhoog@datadoghq.com>
2020-04-23 21:42:25 -0400

    add humandate filter

--- church/templatetags/filters.py
+++ church/templatetags/filters.py
@@ -1,3 +1,4 @@
+import arrow
 from django import template
 
 register = template.Library()
@@ -6,3 +7,9 @@ register = template.Library()
 @register.filter(name="addclass")
 def addclass(value, arg):
     return value.as_widget(attrs={"class": arg})
+
+
+@register.filter(name="humandate")
+def humandate(value):
+    t = arrow.get(value).to("America/Toronto")
+    return t.humanize()
