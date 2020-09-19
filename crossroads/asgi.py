2ee54c3fc2723e4044ac94a7a6dc94c43d4e76d7
kyle-verhoog <kyle@verhoog.ca>
2020-09-19 16:58:30 -0400

    add ddtrace asgi middleware

--- crossroads/asgi.py
+++ crossroads/asgi.py
@@ -2,10 +2,12 @@
 """
 
 import os
+
 import django
 from channels.routing import get_default_application
+from ddtrace.contrib.asgi import TraceMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crossroads.settings.prod")
 django.setup()
 
-application = get_default_application()
+application = TraceMiddleware(get_default_application())
