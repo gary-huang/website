2e8e99a174dd8fcbcc09e0707c96540f36a99615
Jenny <35668149+jenny-zhong@users.noreply.github.com>
2020-10-12 23:08:15 -0400

    code styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,42 @@
-import { useQuery } from 'react-apollo';
+import { useQuery } from 'react-apollo'
 import { hot } from 'react-hot-loader'
-import gql from 'graphql-tag';
-import React from "react";
+import gql from 'graphql-tag'
+import React from 'react'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import { Box, makeStyles } from '@material-ui/core'
 
 export const GET_USER_DATA = gql`
-    query {
-        currentUser {
-            username
-            firstName
-            lastName
-        }
-    }
+	query {
+		currentUser {
+			username
+			firstName
+			lastName
+		}
+	}
 `
+const useStyles = makeStyles({
+	root: {
+		minHeight: '100vh',
+		position: 'relative',
+	},
+})
+
 type AppProps = {}
 
-const AppBase: React.FC<AppProps> = props => {
-    const { data, loading } = useQuery(GET_USER_DATA)
-    return (
-        <h1>{data?.currentUser?.firstName ?? 'kyle'} is a 🍑 dalskjfal</h1>
-    );
+const AppBase: React.FC<AppProps> = (props) => {
+	const { data, loading } = useQuery(GET_USER_DATA)
+	const classes = useStyles()
+	return (
+		<Box className={classes.root}>
+			<Navbar></Navbar>
+			<h1>
+				{data?.currentUser?.firstName ?? 'kyle'}
+				is a 🍑 dalskjfal
+			</h1>
+			<Footer />
+		</Box>
+	)
 }
 
-export const App = hot(module)(AppBase);
+export const App = hot(module)(AppBase)
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,64 +1,65 @@
-import 'react-hot-loader';  // Has to be imported before react + react-dom
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
-import { setContext } from 'apollo-link-context';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { ApolloProvider } from '@apollo/react-hooks';
+import 'react-hot-loader' // Has to be imported before react + react-dom
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import { setContext } from 'apollo-link-context'
+import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import { ApolloProvider } from '@apollo/react-hooks'
 
-import '../index.html';
-import { ThemeProvider, CssBaseline, createMuiTheme } from '@material-ui/core';
+import '../index.html'
+import { ThemeProvider, CssBaseline, createMuiTheme } from '@material-ui/core'
 
 declare global {
-    interface Window {
-        CSRF_TOKEN: string;
-    }
+	interface Window {
+		CSRF_TOKEN: string
+	}
 }
 
 const authLink = setContext((_, { headers }) => {
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: window.CSRF_TOKEN ? `Bearer ${window.CSRF_TOKEN}` : "",
-        }
-    }
-});
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: window.CSRF_TOKEN ? `Bearer ${window.CSRF_TOKEN}` : '',
+		},
+	}
+})
 
 const client = new ApolloClient({
-    link: authLink.concat(ApolloLink.from([
-        onError(({ graphQLErrors, networkError }) => {
-            if (graphQLErrors)
-                graphQLErrors.forEach(({ message, locations, path }) =>
-                    console.log(
-                        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-                    ),
-                );
-            if (networkError) console.log(`[Network error]: ${networkError}`);
-        }),
-        new HttpLink({
-            uri: 'http://localhost:8080/gql/',
-        })
-    ])),
-    cache: new InMemoryCache()
-});
-
+	link: authLink.concat(
+		ApolloLink.from([
+			onError(({ graphQLErrors, networkError }) => {
+				if (graphQLErrors)
+					graphQLErrors.forEach(({ message, locations, path }) =>
+						console.log(
+							`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+						)
+					)
+				if (networkError) console.log(`[Network error]: ${networkError}`)
+			}),
+			new HttpLink({ uri: 'http://localhost:8080/gql/' }),
+		])
+	),
+	cache: new InMemoryCache(),
+})
 
 const theme = createMuiTheme({
-    palette: {
-        type: "dark"
-    },
-});
+	palette: {
+		type: 'light',
+	},
+})
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline>
-                <App />
-            </CssBaseline>
-        </ThemeProvider>
-    </ApolloProvider >, document.getElementById('react-app'));
+	<ApolloProvider client={client}>
+		<ThemeProvider theme={theme}>
+			<CssBaseline>
+				<App />
+			</CssBaseline>
+		</ThemeProvider>
+	</ApolloProvider>,
+	document.getElementById('react-app')
+)
