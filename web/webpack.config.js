9b13461d042e499c96775941e727aa79745a6537
Jenny <35668149+jenny-zhong@users.noreply.github.com>
2020-10-12 23:05:07 -0400

    styling

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,70 +1,89 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: ['./src/index'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.js',
-  },
-  resolve: {
-    modules: ['node_modules'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom',
-    },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  devServer: {
-    contentBase: './dist',
-    proxy: {
-      '/gql': {
-        target: {
-          host: '0.0.0.0',
-          protocol: 'http:',
-          port: 8000
-        },
-        // pathRewrite: {
-        //   '^/gql': ''
-        // }
-      }
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: ['html-loader'],
-      },
-      {
-        test: /\.(j|t)s(x)?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            babelrc: false,
-            presets: [
-              [
-                '@babel/preset-env',
-                { targets: { browsers: 'last 2 versions' } }, // or whatever your project requires
-              ],
-              '@babel/preset-typescript',
-              '@babel/preset-react',
-            ],
-            plugins: [
-              // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-              // ['@babel/plugin-proposal-decorators', { legacy: true }],
-              ['@babel/plugin-proposal-class-properties', { loose: true }],
-              'react-hot-loader/babel',
-            ],
-          },
-        },
-      },
-    ],
-  },
-  devtool: 'eval-source-map',
-  plugins: [new ForkTsCheckerWebpackPlugin(), new webpack.NamedModulesPlugin(), new HtmlWebpackPlugin({ template: 'index.html' })],
-};
+	mode: 'development',
+	entry: ['./src/index'],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'app.js',
+	},
+	resolve: {
+		modules: ['node_modules'],
+		alias: {
+			'react-dom': '@hot-loader/react-dom',
+		},
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+	},
+	devServer: {
+		contentBase: './dist',
+		proxy: {
+			'/gql': {
+				target: {
+					host: '0.0.0.0',
+					protocol: 'http:',
+					port: 8000,
+				},
+				// pathRewrite: {   '^/gql': '' }
+			},
+			'/static': {
+				target: {
+					host: '0.0.0.0',
+					protocol: 'http:',
+					port: 8000,
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/i,
+				loader: ['html-loader'],
+			},
+			{
+				test: /\.(j|t)s(x)?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+						babelrc: false,
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									targets: {
+										browsers: 'last 2 versions',
+									},
+								}, // or whatever your project requires
+							],
+							'@babel/preset-typescript',
+							'@babel/preset-react',
+						],
+						plugins: [
+							// plugin-proposal-decorators is only needed if you're using experimental
+							// decorators in TypeScript ['@babel/plugin-proposal-decorators', { legacy: true
+							// }],
+							[
+								'@babel/plugin-proposal-class-properties',
+								{
+									loose: true,
+								},
+							],
+							'react-hot-loader/babel',
+						],
+					},
+				},
+			},
+		],
+	},
+	devtool: 'eval-source-map',
+	plugins: [
+		new ForkTsCheckerWebpackPlugin(),
+		new webpack.NamedModulesPlugin(),
+		new HtmlWebpackPlugin({ template: 'index.html' }),
+	],
+}
