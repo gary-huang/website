1b3da69f3d195079ef8f9d8a55efb12b31e159da
Jenny Zhong <jenny.zhong@hotmail.com>
2021-02-16 22:22:11 -0500

    WIP: enable image imports

--- web/Home.tsx
+++ web/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Box, Container, makeStyles } from "@material-ui/core";
+// import Background from './assets/church-home.jpg'
 
 const useStyles = makeStyles({
   root: {
     minHeight: "100vh",
     position: "relative",
+    // backgroundImage:`url(${Background})`,
+    backgrounSize: 'cover'
   },
 });
 
@@ -15,8 +18,9 @@ const Home: React.FC<HomeProps> = () => {
   return (
     <Container>
       <Box className={classes.root}>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
+        {/* <h1>Hello</h1>
+        <h1>Hello</h1> */}
+        <img src={require('/static/img/crossroads.png')} />
       </Box>
     </Container>
   );
--- webpack.dev.js
+++ webpack.dev.js
@@ -50,6 +50,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000' 
+      }
     ],
   },
   devtool: "eval-source-map",
